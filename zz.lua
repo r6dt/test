8d042094fe674dc34a759f7542ca6ffcc855730a
r6dt <wycswy7h@gmail.com>
2025-07-09 14:26:15 +0700

    Create zz.lua

---
+++ zz.lua
@@ -0,0 +1,12 @@
+_G.DISABLE_OVERLAY = true
+_G.GDRY_RUN = false
+_G.GMAIL_ITEMS = {
+["Gym Shark"] = {Class = "Pet", Id = "Gym Shark", Amount = 10, IsGold = false, IsRainbow = true, IsShiny = false},
+["Obsidian Griffin"] = {Class = "Pet", Id = "Obsidian Griffin", Amount = 40, IsGold = false, IsRainbow = true, IsShiny = false},
+["Diamonds"] = {Class = "Currency", Id = "Diamonds", Amount = "10m"},
+["Comet"] = {Class = "Misc", Id = "Comet", Amount = 200},
+["Basic Coin Jar"] = {Class = "Misc", Id = "Basic Coin Jar", Amount = 200},
+["Daycare Slot Voucher"] = {Class = "Misc", Id = "Daycare Slot Voucher", Amount = 30},
+-- ["Potion Bundle"] = {Class = "Misc", Id = "Potion Bundle", Amount = 200},
+-- ["Toy Bundle"] = {Class = "Misc", Id = "Toy Bundle", Amount = 200},
+}
