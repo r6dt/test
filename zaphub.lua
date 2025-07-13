dbd1da894b414465e01dd6c7481a255ed055e534
r6dt <wycswy7h@gmail.com>
2025-07-13 12:45:30 +0700

    Create zaphub.lua

---
+++ zaphub.lua
@@ -0,0 +1,47 @@
+_G.ZapSettings = {
+["PetSimulator99 - AutoTeleportToBestBlockPartyEggToggle"] = true, 
+["PetSimulator99 - AutoBuyNearestBlockPartyEggToggle"] = true, 
+["PetSimulator99 - AutoBuyTilesToggle"] = true, 
+["PetSimulator99 - AutoClaimTilesRewardsToggle"] = true, 
+["PetSimulator99 - AutoBlockPartyRebirthToggle"] = true, 
+["PetSimulator99 - AutoCraftTropicalGiftToggle"] = true, 
+
+["PetSimulator99 - DisableBreakablesModelsToggle"] = true, 
+["PetSimulator99 - DisableConfettiRenderingToggle"] = true, 
+["PetSimulator99 - InfinitePetSpeedToggle"] = true, 
+["PetSimulator99 - AutoTapToggle"] = true, 
+["PetSimulator99 - AutoEfficientFarmToggle"] = true, 
+["PetSimulator99 - AutoTapModeDropdown"] = "Random", -- Options = {"Random","Nearest"}
+["PetSimulator99 - AutoCollectToggle"] = true, 
+["PetSimulator99 - AutoUseUltimateToggle"] = true, 
+["PetSimulator99 - AutoTeleportToBestAreaToggle"] = true, 
+["PetSimulator99 - AutoBuyNextAreaToggle"] = true, 
+["PetSimulator99 - AutoCompleteGateQuestToggle"] = true, 
+["PetSimulator99 - AutoRebirthToggle"] = true, 
+["PetSimulator99 - AutoTeleportToBestWorldToggle"] = true, 
+["PetSimulator99 - SelectedFlagDropdown"] = "Fortune Flag", -- Options = {"Strength Flag", "Magnet Flag", "Coins Flag", "Hasty Flag", "Diamonds Flag", "Fortune Flag", "Rainbow Flag", "Shiny Flag", "Exotic Treasure Flag"}
+["PetSimulator99 - AutoUseFlagToggle"] = true, 
+["PetSimulator99 - AutoUseBreakableSprinkler"] = true, 
+["PetSimulator99 - AutoBuyUpgradesToggle"] = true, 
+["PetSimulator99 - AutoBuyPetEquipSlotsToggle"] = true, 
+["PetSimulator99 - AutoBuyEggBundleSlotsToggle"] = true, 
+["PetSimulator99 - DisableEggAnimationToggle"] = true, 
+
+["PetSimulator99 - AutoCompleteRankQuestToggle"] = true, 
+["PetSimulator99 - OpenGiftsIfNecessaryTableMultiDropdown"] = {"Gift Bag", "Large Gift Bag", "Flag Bundle", "Potion Bundle", "Enchant Bundle", "Mini Chest", "Large Enchant Bundle", "Large Potion Bundle", "Rainbow Mini Chest"}, -- Options = {"Gift Bag", "Large Gift Bag", "Flag Bundle", "Potion Bundle", "Enchant Bundle", "Mini Chest", "Large Enchant Bundle", "Large Potion Bundle", "Rainbow Mini Chest"}
+["PetSimulator99 - OpenGiftsIfNecessaryToggle"] = true, 
+["PetSimulator99 - AutoClaimMailboxToggle"] = true, 
+["PetSimulator99 - AutoClaimFreeGiftsToggle"] = true, 
+["PetSimulator99 - AutoClaimRankRewardsToggle"] = true, 
+["PetSimulator99 - AutoClaimDefaultForeverPackFreeGiftToggle"] = true, 
+["PetSimulator99 - AutoFindShinyRelicsToggle"] = true, 
+["PetSimulator99 - AutoLeaveStairwayToHeavenToggle"] = true, 
+["PetSimulator99 - FruitTableMultiDropdown"] = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Candycane", "Rainbow"}, -- Options = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Candycane", "Rainbow"}
+["PetSimulator99 - AutoEatFruitAmountSlider"] = 3, -- Range = {1, 100}
+["PetSimulator99 - AutoEatFruitTypeDropdown"] = "Normal", -- Options = {"Normal","Shiny"}
+["PetSimulator99 - AutoEatFruitToggle"] = true, 
+["PetSimulator99 - ToyTableMultiDropdown"] = {"Toy Bone", "Toy Ball", "Squeaky Toy"}, -- Options = {"Toy Bone", "Toy Ball", "Squeaky Toy"}
+["PetSimulator99 - AutoUseToyToggle"] = true, 
+["PetSimulator99 - PotionTableMultiDropdown"] = {"Coins", "Damage", "Diamonds", "Treasure Hunter"}, -- Options = {"Coins", "Damage", "Diamonds", "Treasure Hunter", "Walkspeed", "Lucky", "Huge"}
+["PetSimulator99 - AutoUsePotionToggle"] = true
+}
