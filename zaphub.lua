efaf3244fb791e2464429014117b997185089b21
r6dt <wycswy7h@gmail.com>
2025-07-13 12:50:45 +0700

    Update zaphub.lua

--- zaphub.lua
+++ zaphub.lua
@@ -14,11 +14,6 @@ _G.ZapSettings = {
 ["PetSimulator99 - AutoTapModeDropdown"] = "Random", -- Options = {"Random","Nearest"}
 ["PetSimulator99 - AutoCollectToggle"] = true, 
 ["PetSimulator99 - AutoUseUltimateToggle"] = true, 
-["PetSimulator99 - AutoTeleportToBestAreaToggle"] = true, 
-["PetSimulator99 - AutoBuyNextAreaToggle"] = true, 
-["PetSimulator99 - AutoCompleteGateQuestToggle"] = true, 
-["PetSimulator99 - AutoRebirthToggle"] = true, 
-["PetSimulator99 - AutoTeleportToBestWorldToggle"] = true, 
 ["PetSimulator99 - SelectedFlagDropdown"] = "Fortune Flag", -- Options = {"Strength Flag", "Magnet Flag", "Coins Flag", "Hasty Flag", "Diamonds Flag", "Fortune Flag", "Rainbow Flag", "Shiny Flag", "Exotic Treasure Flag"}
 ["PetSimulator99 - AutoUseFlagToggle"] = true, 
 ["PetSimulator99 - AutoUseBreakableSprinkler"] = true, 
@@ -26,23 +21,7 @@ _G.ZapSettings = {
 ["PetSimulator99 - AutoBuyPetEquipSlotsToggle"] = true, 
 ["PetSimulator99 - AutoBuyEggBundleSlotsToggle"] = true, 
 ["PetSimulator99 - DisableEggAnimationToggle"] = true, 
-
-["PetSimulator99 - AutoCompleteRankQuestToggle"] = true, 
-["PetSimulator99 - OpenGiftsIfNecessaryTableMultiDropdown"] = {"Gift Bag", "Large Gift Bag", "Flag Bundle", "Potion Bundle", "Enchant Bundle", "Mini Chest", "Large Enchant Bundle", "Large Potion Bundle", "Rainbow Mini Chest"}, -- Options = {"Gift Bag", "Large Gift Bag", "Flag Bundle", "Potion Bundle", "Enchant Bundle", "Mini Chest", "Large Enchant Bundle", "Large Potion Bundle", "Rainbow Mini Chest"}
-["PetSimulator99 - OpenGiftsIfNecessaryToggle"] = true, 
-["PetSimulator99 - AutoClaimMailboxToggle"] = true, 
-["PetSimulator99 - AutoClaimFreeGiftsToggle"] = true, 
-["PetSimulator99 - AutoClaimRankRewardsToggle"] = true, 
-["PetSimulator99 - AutoClaimDefaultForeverPackFreeGiftToggle"] = true, 
-["PetSimulator99 - AutoFindShinyRelicsToggle"] = true, 
-["PetSimulator99 - AutoLeaveStairwayToHeavenToggle"] = true, 
-["PetSimulator99 - FruitTableMultiDropdown"] = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Candycane", "Rainbow"}, -- Options = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Candycane", "Rainbow"}
-["PetSimulator99 - AutoEatFruitAmountSlider"] = 3, -- Range = {1, 100}
-["PetSimulator99 - AutoEatFruitTypeDropdown"] = "Normal", -- Options = {"Normal","Shiny"}
-["PetSimulator99 - AutoEatFruitToggle"] = true, 
-["PetSimulator99 - ToyTableMultiDropdown"] = {"Toy Bone", "Toy Ball", "Squeaky Toy"}, -- Options = {"Toy Bone", "Toy Ball", "Squeaky Toy"}
-["PetSimulator99 - AutoUseToyToggle"] = true, 
-["PetSimulator99 - PotionTableMultiDropdown"] = {"Coins", "Damage", "Diamonds", "Treasure Hunter"}, -- Options = {"Coins", "Damage", "Diamonds", "Treasure Hunter", "Walkspeed", "Lucky", "Huge"}
-["PetSimulator99 - AutoUsePotionToggle"] = true
 }
+
+script_key="JlpKitloGXHLUcHNMaaPACsamFEkGWXL";
 loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
