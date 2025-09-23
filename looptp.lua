928f66edcd67623527c5dc61390be6789cdead69
r6dt <wycswy7h@gmail.com>
2025-09-24 03:23:30 +0700

    Update looptp.lua

--- looptp.lua
+++ looptp.lua
@@ -20,7 +20,7 @@ local function isNear(pos1, pos2, tol)
     return (pos1 - pos2).Magnitude <= tol
 end
 
--- Loop เช็คทุก 5 วิ
+-- Loop เช็คทุก 300 วิ
 while task.wait(300) do
     char = player.Character
     hrp = char and char:FindFirstChild("HumanoidRootPart")
